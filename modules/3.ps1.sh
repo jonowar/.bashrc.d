f0fb496210c422302984922ebff87b8e4e50f6ec
Jono Warren <jono.warren@reddit.com>
2017-08-08 18:40:01 -0700

    configure PS1 with git_prompt

--- modules/3.ps1.sh
+++ modules/3.ps1.sh
@@ -1,8 +1,2 @@
 # PS1 terminal prompt coloring
-pre_ps1="$HI_Cyan\u$Purple@$HI_Green\h:$HI_Blue\w$Color_Off"
-PS1=$pre_ps1' $(\
-  if [ -n "$(git status -z 2>/dev/null)" ]; then \
-    echo "'$Red'"; \
-  else \
-    echo "'$Green'"; \
-  fi)'"\$(git rev-parse --abbrev-ref HEAD 2>/dev/null)$Color_Off$ "
+PS1="$HI_Cyan\u$Purple@$HI_Green\h:$HI_Blue\w$Color_Off"
--- modules/5.git.sh
+++ modules/5.git.sh
@@ -1,20 +1,39 @@
 # git autocompletion
-if [ -f ~/.git-completion.bash ]; then
-  source ~/.git-completion.bash
+git_completion_file=$HOME/.git-completion.bash
+git_completion_url="https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
+
+if [ ! -f $git_completion_file ]; then
+    echo "
+** git autocompletion not installed. installing from $git_completion_url..."
+    curl -s -o $git_completion_file $git_completion_url
+    echo "** done.
+"
 fi
+source $git_completion_file
 
-# sweeeeet PS1 for git status junk
-function git_prompt(){
-    local branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-    if [ -n "$branch" ]
-    then
-        if [ -n "$(git status -z 2>/dev/null)" ]
-        then
-            printf "\033[31m{$branch}\033[0m "
-        else
-            printf "\033[32m($branch)\033[0m "
-        fi
-    fi
-}
 
+# git prompt
+git_prompt_file=$HOME/.git-prompt.sh
+git_prompt_url="https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh"
+
+if [ ! -f $git_prompt_file ]; then
+    echo "
+** git prompt not installed. installing from $git_prompt_url..."
+    curl -s -o $git_prompt_file $git_prompt_url
+    echo "** done.
+"
+fi
+source $git_prompt_file
+
+# configuring git ps1
+git_prompt_ps1='$(__git_ps1 "(%s)")\$ '
+PROMPT_COMMAND="__git_ps1 \"$PS1\" \"\\\$ \""
+GIT_PS1_SHOWCOLORHINTS=true # use color in git_ps1
+GIT_PS1_SHOWDIRTYSTATE=true # * = unstaged, + = staged
+GIT_PS1_SHOWSTASHSTATE=true # $ = something stashed
+GIT_PS1_SHOWUNTRACKEDFILES=true # % if something untracked
+GIT_PS1_HIDE_IF_PWD_IGNORED=true # hide git_ps1 in .gitignore'd directories
+GIT_PS1_SHOWUPSTREAM="auto" # < = behind, > = ahead, <> = diverged, = = no difference
+
+# alias lg command to show git log in a pretty format
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
